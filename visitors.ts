0e7ecf2c5fc81f70fd0dcfa037296198f03747e8
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-03-29 15:02:49 +0300

    WIP: Add a few more visitor functions

--- visitors.ts
+++ visitors.ts
@@ -1,33 +1,70 @@
 import StellaVisitor from './src/stella/stellaParserVisitor';
-import { DeclFunContext, ProgramContext } from './src/stella/stellaParser';
+import {
+  DeclContext,
+  DeclFunContext,
+  DeclTypeAliasContext,
+  ExtensionContext,
+  ProgramContext,
+  type AnExtensionContext,
+} from './src/stella/stellaParser';
 
 export class AstPrinter extends StellaVisitor<void> {
   private depth = 0;
   private print(msg: string) {
-    return '|'.repeat(this.depth) + '> ' + msg;
+    console.log('|'.repeat(this.depth) + '> ' + msg);
   }
 
   visitProgram = (ctx: ProgramContext) => {
     this.depth++;
 
     this.print('Program');
+    ctx.extension_list().forEach(this.visitAnExtension);
+    ctx.decl_list().forEach(this.visitDecl);
 
-    ctx.extension_list().forEach((ext) => {
-      // TODO: do something with the extension
-    });
-    ctx.decl_list().forEach((decl) => {
-      // TODO: do something with the declaration
+    this.depth--;
+  };
+
+  visitAnExtension = (ext: ExtensionContext) => {
+    const extension = ext as AnExtensionContext;
+    this.depth++;
+
+    extension.ExtensionName_list().forEach((name) => {
+      this.print(`Extension [${name.getText()}]`);
     });
 
     this.depth--;
   };
 
+  visitDecl = (ctx: DeclContext) => {
+    console.log('Decl');
+
+    if (ctx instanceof DeclFunContext) {
+      return ctx.accept(this);
+    }
+    if (ctx instanceof DeclTypeAliasContext) {
+      return ctx.accept(this);
+    } else {
+      this.print('Declaration');
+    }
+  };
+
   visitDeclFun = (ctx: DeclFunContext) => {
     this.depth++;
 
-    this.print(`Function [${ctx.StellaIdent().getText()}]`);
+    this.print(`FunctionDecl [${ctx.StellaIdent().getText()}]`);
 
     // TODO: visit the children
+
+    this.depth--;
+  };
+
+  visitDeclTypeAlias = (ctx: DeclTypeAliasContext) => {
+    this.depth++;
+
+    this.print('Type Alias');
+
+    ctx._name;
+    ctx._atype;
 
     this.depth--;
   };
