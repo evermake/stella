b5069a67c17df0c4b5e554643985d024e3f9cc90
Vladislav Deryabkin <v.deryabkin@innopolis.university>
2024-02-08 12:52:06 +0300

    refactor context

---
+++ src/context.ts
@@ -0,0 +1,44 @@
+import type { Type } from './ast'
+import type { Extension } from './types'
+
+export class Context {
+  #parent: Context | null
+  #symbols: Map<string, Type>
+  #extensions: Set<Extension>
+
+  readonly scope: {
+    get(name: string): Type | undefined
+    set(name: string, type: Type): void
+    has(name: string): boolean
+  }
+
+  readonly extensions: {
+    has(ext: Extension): boolean
+    add(ext: Extension): void
+  }
+
+  constructor(parent: Context | null = null) {
+    this.#parent = parent
+    this.#symbols = new Map()
+    this.#extensions = new Set()
+
+    this.scope = {
+      get: name => (this.#symbols.get(name) ?? this.#parent?.scope.get(name)),
+      set: (name, type) => {
+        this.#symbols.set(name, type)
+      },
+      has: name => this.#symbols.has(name) || (this.#parent?.scope.has(name) ?? false),
+    }
+
+    this.extensions = {
+      has: ext => this.#extensions.has(ext) || (this.#parent?.extensions.has(ext) ?? false),
+      add: (ext) => {
+        this.#extensions.add(ext)
+      },
+    }
+  }
+
+  newChild(): Context {
+    return new Context(this)
+  }
+}
--- src/typechecker.ts
+++ src/typechecker.ts
@@ -2,12 +2,12 @@ import type { DeclFun, Expr, Program, Type } from './ast'
 import { TypecheckingFailedError } from './errors'
 import type { Extension } from './types'
 import { T, areTypesEqual, t } from './utils'
+import { Context } from './context'
 
 export function typecheckProgram(ast: Program) {
   const ctx = new Context()
-  const globalScope = new Scope(null, ctx)
 
-  ast.extensions.forEach(ext => ctx.addExtension(ext as Extension))
+  ast.extensions.forEach(ext => ctx.extensions.add(ext as Extension))
 
   // Add all global functions to the scope, so we can correctly typecheck their
   // bodies later, when their calls will be invoked.
@@ -16,7 +16,7 @@ export function typecheckProgram(ast: Program) {
       if (!decl.returnType) {
         throw new TypecheckingFailedError('ERROR_MISSING_EXPLICIT_RETURN_TYPE', `Function "${decl.name}" is missing an explicit return type.`)
       }
-      globalScope.set(decl.name, {
+      ctx.scope.set(decl.name, {
         type: 'TypeFun',
         parametersTypes: decl.parameters.map(p => p.paramType),
         returnType: decl.returnType,
@@ -27,20 +27,20 @@ export function typecheckProgram(ast: Program) {
   for (const decl of ast.declarations) {
     switch (decl.type) {
       case 'DeclFun':
-        typecheckDeclFun(decl, globalScope)
+        typecheckDeclFun(decl, ctx)
         break
       default:
         throw new Error(`Typechecking for declaration of type "${decl.type}" is not supported.`)
     }
   }
 
-  checkForValidMainInGlobalScope(globalScope)
+  checkForValidMainInContext(ctx)
 }
 
-function typecheckDeclFun(decl: DeclFun, parentScope: Scope) {
+function typecheckDeclFun(decl: DeclFun, ctx: Context) {
   // @todo: decl.annations, decl.nestedDeclarations, decl.throwTypes
 
-  const localScope = new Scope(parentScope, parentScope.ctx)
+  const localCtx = ctx.newChild()
 
   if (decl.parameters.length !== 1) {
     throw new Error(`Function declarations must have exactly one parameter, but "${decl.name}" has ${decl.parameters.length}.`)
@@ -51,21 +51,21 @@ function typecheckDeclFun(decl: DeclFun, parentScope: Scope) {
     ? decl.returnValue.expr1
     : decl.returnValue
 
-  localScope.identifiers.set(param.name, param.paramType)
+  localCtx.scope.set(param.name, param.paramType)
 
   if (decl.returnType) {
-    deriveType(returnVal, localScope, decl.returnType)
+    deriveType(returnVal, localCtx, decl.returnType)
   }
 }
 
-function deriveType(expr: Expr, scope: Scope, wantedType?: Type): Type {
+function deriveType(expr: Expr, ctx: Context, wantedType?: Type): Type {
   const derivedType = (() => {
     switch (expr.type) {
       case 'ConstBool': {
         return T.Bool
       }
       case 'ConstInt': {
-        if (scope.ctx.hasExtension('#natural-literals')) {
+        if (ctx.extensions.has('#natural-literals')) {
           if (expr.value < 0) {
             throw new TypecheckingFailedError('ERROR_ILLEGAL_NEGATIVE_LITERAL', `Negative integers are not supported (got ${expr.value}).`)
           }
@@ -77,35 +77,35 @@ function deriveType(expr: Expr, scope: Scope, wantedType?: Type): Type {
         return T.Nat
       }
       case 'Var': {
-        const varType = scope.get(expr.name)
+        const varType = ctx.scope.get(expr.name)
         if (!varType) {
           throw new TypecheckingFailedError('ERROR_UNDEFINED_VARIABLE', `Variable "${expr.name}" is not defined.`)
         }
         return varType
       }
       case 'Succ': {
-        deriveType(expr.expr, scope, T.Nat)
+        deriveType(expr.expr, ctx, T.Nat)
         return T.Nat
       }
       case 'NatIsZero': {
-        deriveType(expr.expr, scope, T.Nat)
+        deriveType(expr.expr, ctx, T.Nat)
         return T.Bool
       }
       case 'NatRec': {
-        deriveType(expr.n, scope, T.Nat)
+        deriveType(expr.n, ctx, T.Nat)
 
-        const initialType = deriveType(expr.initial, scope)
-        deriveType(expr.step, scope, T.fn`${T.Nat} -> ${
+        const initialType = deriveType(expr.initial, ctx)
+        deriveType(expr.step, ctx, T.fn`${T.Nat} -> ${
           T.fn`${initialType} -> ${initialType}`
         }`)
 
         return initialType
       }
       case 'If': {
-        deriveType(expr.condition, scope, T.Bool)
+        deriveType(expr.condition, ctx, T.Bool)
 
-        const thenType = deriveType(expr.thenExpr, scope)
-        deriveType(expr.elseExpr, scope, thenType)
+        const thenType = deriveType(expr.thenExpr, ctx)
+        deriveType(expr.elseExpr, ctx, thenType)
 
         return thenType
       }
@@ -116,7 +116,7 @@ function deriveType(expr: Expr, scope: Scope, wantedType?: Type): Type {
         const argExpr = expr.arguments[0]
         const fnExpr = expr.function
 
-        const fnType = deriveType(fnExpr, scope)
+        const fnType = deriveType(fnExpr, ctx)
         if (fnType.type !== 'TypeFun') {
           throw new TypecheckingFailedError('ERROR_NOT_A_FUNCTION', `Left side of application must be a function, but got ${t(fnType)}.`)
         }
@@ -125,7 +125,7 @@ function deriveType(expr: Expr, scope: Scope, wantedType?: Type): Type {
           throw new Error(`Functions must accept exactly one parameter.`)
         }
 
-        deriveType(argExpr, scope, fnType.parametersTypes[0])
+        deriveType(argExpr, ctx, fnType.parametersTypes[0])
 
         return fnType.returnType
       }
@@ -151,16 +151,16 @@ function deriveType(expr: Expr, scope: Scope, wantedType?: Type): Type {
           wantedReturnType = wantedType.returnType
         }
 
-        const nestedScope = new Scope(scope, scope.ctx)
-        nestedScope.set(paramDecl.name, paramDecl.paramType)
+        const nestedCtx = ctx.newChild()
+        nestedCtx.scope.set(paramDecl.name, paramDecl.paramType)
 
-        return T.fn`${paramDecl.paramType} -> ${deriveType(expr.returnValue, nestedScope, wantedReturnType)}`
+        return T.fn`${paramDecl.paramType} -> ${deriveType(expr.returnValue, nestedCtx, wantedReturnType)}`
       }
       case 'Sequence': {
         if (expr.expr2) {
           throw new Error('Sequences are not supported.')
         }
-        return deriveType(expr.expr1, scope, wantedType)
+        return deriveType(expr.expr1, ctx, wantedType)
       }
       default:
         throw new Error(`Cannot derive type for expression "${expr.type}".`)
@@ -174,47 +174,8 @@ function deriveType(expr: Expr, scope: Scope, wantedType?: Type): Type {
   return derivedType
 }
 
-class Context {
-  enabledExtensions: Set<Extension> = new Set()
-
-  addExtension(extension: Extension) {
-    this.enabledExtensions.add(extension)
-  }
-
-  hasExtension(extension: Extension) {
-    return this.enabledExtensions.has(extension)
-  }
-}
-
-class Scope {
-  identifiers: Map<string, Type> = new Map()
-
-  constructor(
-    public parent: Scope | null = null,
-    public ctx: Context,
-  ) {}
-
-  get(name: string, traverseParents = true): Type | undefined {
-    if (this.identifiers.has(name)) {
-      return this.identifiers.get(name)!
-    }
-    if (traverseParents && this.parent) {
-      return this.parent.get(name)
-    }
-    return undefined
-  }
-
-  set(name: string, type: Type) {
-    this.identifiers.set(name, type)
-  }
-
-  has(name: string, traverseParents = true): boolean {
-    return this.get(name, traverseParents) != null
-  }
-}
-
-function checkForValidMainInGlobalScope(scope: Scope) {
-  const main = scope.identifiers.get('main')
+function checkForValidMainInContext(ctx: Context) {
+  const main = ctx.scope.get('main')
   if (!main) {
     throw new TypecheckingFailedError('ERROR_MISSING_MAIN', '"main" function is not found in the scope.')
   }
