3d8324bb99c63007862a1aeca48f2f5e26e58df9
Vladislav Deryabkin <v.deryabkin@innopolis.university>
2024-02-09 18:16:54 +0300

    refactor typechecking, remove check for function parameters length

--- src/typechecker.ts
+++ src/typechecker.ts
@@ -42,10 +42,6 @@ function typecheckDeclFun(decl: DeclFun, ctx: Context) {
 
   const localCtx = ctx.newChild()
 
-  if (decl.parameters.length !== 1) {
-    throw new Error(`Function declarations must have exactly one parameter, but "${decl.name}" has ${decl.parameters.length}.`)
-  }
-
   const param = decl.parameters[0]
   const returnVal = decl.returnValue.type === 'Sequence'
     ? decl.returnValue.expr1
@@ -54,12 +50,20 @@ function typecheckDeclFun(decl: DeclFun, ctx: Context) {
   localCtx.scope.set(param.name, param.paramType)
 
   if (decl.returnType) {
-    deriveType(returnVal, localCtx, decl.returnType)
+    inferType({ expr: returnVal, ctx: localCtx, expectedType: decl.returnType })
   }
 }
 
-function deriveType(expr: Expr, ctx: Context, wantedType?: Type): Type {
-  const derivedType = (() => {
+function inferType({
+  expr,
+  ctx,
+  expectedType,
+}: {
+  expr: Expr
+  ctx: Context
+  expectedType?: Type
+}): Type {
+  const inferredType = (() => {
     switch (expr.type) {
       case 'ConstBool': {
         return T.Bool
@@ -84,92 +88,81 @@ function deriveType(expr: Expr, ctx: Context, wantedType?: Type): Type {
         return varType
       }
       case 'Succ': {
-        deriveType(expr.expr, ctx, T.Nat)
+        inferType({ expr, ctx, expectedType: T.Nat })
         return T.Nat
       }
       case 'NatIsZero': {
-        deriveType(expr.expr, ctx, T.Nat)
+        inferType({ expr, ctx, expectedType: T.Nat })
         return T.Bool
       }
       case 'NatRec': {
-        deriveType(expr.n, ctx, T.Nat)
+        inferType({ expr: expr.n, ctx, expectedType: T.Nat })
 
-        const initialType = deriveType(expr.initial, ctx)
-        deriveType(expr.step, ctx, T.fn([T.Nat], T.fn([initialType], initialType)))
+        const initialType = inferType({ expr: expr.initial, ctx })
+        const expectedStepType = T.fn([T.Nat], T.fn([initialType], initialType))
+        inferType({ expr: expr.step, ctx, expectedType: expectedStepType })
 
         return initialType
       }
       case 'If': {
-        deriveType(expr.condition, ctx, T.Bool)
+        inferType({ expr: expr.condition, ctx, expectedType: T.Bool })
 
-        const thenType = deriveType(expr.thenExpr, ctx)
-        deriveType(expr.elseExpr, ctx, thenType)
+        const thenType = inferType({ expr: expr.thenExpr, ctx })
+        inferType({ expr: expr.elseExpr, ctx, expectedType: thenType })
 
         return thenType
       }
       case 'Application': {
-        if (expr.arguments.length !== 1) {
-          throw new Error(`Functions must accept exactly one parameter.`)
-        }
-        const argExpr = expr.arguments[0]
-        const fnExpr = expr.function
-
-        const fnType = deriveType(fnExpr, ctx)
+        const fnType = inferType({ expr: expr.function, ctx })
         if (fnType.type !== 'TypeFun') {
           throw new TypecheckingFailedError('ERROR_NOT_A_FUNCTION', `Left side of application must be a function, but got ${t(fnType)}.`)
         }
 
-        if (fnType.parametersTypes.length !== 1) {
-          throw new Error(`Functions must accept exactly one parameter.`)
-        }
-
-        deriveType(argExpr, ctx, fnType.parametersTypes[0])
+        inferType({ expr: expr.arguments[0], ctx, expectedType: fnType.parametersTypes[0] })
 
         return fnType.returnType
       }
       case 'Abstraction': {
-        if (expr.parameters.length !== 1) {
-          throw new Error(`Anonymous function must have exactly one parameter, but definition has ${expr.parameters.length}.`)
-        }
+        const param = expr.parameters[0]
 
-        const paramDecl = expr.parameters[0]
+        let expectedReturnType
+        if (expectedType) {
+          if (expectedType.type !== 'TypeFun') {
+            throw new TypecheckingFailedError('ERROR_UNEXPECTED_LAMBDA', `Expected ${t(expectedType)}, but got lambda.`)
+          }
 
-        let wantedReturnType
-        if (wantedType) {
-          if (wantedType.type !== 'TypeFun') {
-            throw new TypecheckingFailedError('ERROR_UNEXPECTED_LAMBDA', `Expected ${t(wantedType)}, but got lambda.`)
+          const wantedParamType = expectedType.parametersTypes[0]
+          if (!areTypesEqual(wantedParamType, param.paramType)) {
+            throw new TypecheckingFailedError('ERROR_UNEXPECTED_TYPE_FOR_PARAMETER', `Unexpected type for parameter "${param.name}": expected ${t(wantedParamType)}, but got ${t(param.paramType)}.`)
           }
-          if (wantedType.parametersTypes.length !== 1) {
-            throw new Error(`Functions must accept exactly one parameter.`)
-          }
-          const wantedParamType = wantedType.parametersTypes[0]
-          if (!areTypesEqual(wantedParamType, paramDecl.paramType)) {
-            throw new TypecheckingFailedError('ERROR_UNEXPECTED_TYPE_FOR_PARAMETER', `Unexpected type for parameter "${paramDecl.name}": expected ${t(wantedParamType)}, but got ${t(paramDecl.paramType)}.`)
-          }
-          wantedReturnType = wantedType.returnType
+
+          expectedReturnType = expectedType.returnType
         }
 
         const nestedCtx = ctx.newChild()
-        nestedCtx.scope.set(paramDecl.name, paramDecl.paramType)
+        nestedCtx.scope.set(param.name, param.paramType)
 
-        return T.fn([paramDecl.paramType], deriveType(expr.returnValue, nestedCtx, wantedReturnType))
+        return T.fn(
+          [param.paramType],
+          inferType({ expr: expr.returnValue, ctx: nestedCtx, expectedType: expectedReturnType }),
+        )
       }
       case 'Sequence': {
         if (expr.expr2) {
           throw new Error('Sequences are not supported.')
         }
-        return deriveType(expr.expr1, ctx, wantedType)
+        return inferType({ expr: expr.expr1, ctx, expectedType })
       }
       default:
         throw new Error(`Cannot derive type for expression "${expr.type}".`)
     }
   })()
 
-  if (wantedType) {
-    expect(derivedType).toBe(wantedType)
+  if (expectedType) {
+    expect(inferredType).toBe(expectedType)
   }
 
-  return derivedType
+  return inferredType
 }
 
 function checkForValidMainInContext(ctx: Context) {
